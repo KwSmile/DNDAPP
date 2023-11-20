44d47b05759d2d2c39e20bfe920ee685719e9d10
Deividas-BlueSkin <somecrazyp@gmail.com>
2023-11-20 22:27:15 +0200

    Sheet setup

--- app/src/App.js
+++ app/src/App.js
@@ -1,10 +1,13 @@
 import DicePage from "./AppParts/DicePage/DicePage";
+import SheetPage from "./AppParts/SheetPage/SheetPage";
+import style from "./App.module.scss"
 
 function App() {
   return (
-    <>
+    <div className={style.container}>
       <DicePage />
-    </>
+      <SheetPage />
+    </div>
   )
 }
 
---
+++ app/src/AppParts/SheetPage/SheetPage.js
@@ -0,0 +1,8 @@
+
+export default function SheetPage() {
+    return (
+        <div>
+            <textarea></textarea>
+        </div>
+    )
+}

17d08213ac9e7d133e9b8a547f7b5298fe577f85
Deividas-BlueSkin <somecrazyp@gmail.com>
2023-11-22 14:00:51 +0200

    form work

--- app/src/AppParts/SheetPage/SheetPage.js
+++ app/src/AppParts/SheetPage/SheetPage.js
@@ -1,8 +1,29 @@
+import { useForm } from 'react-hook-form';
+import style from './SheetPage.module.scss'
 
 export default function SheetPage() {
+    const {
+        register,
+        handleSubmit,
+        watch,
+        // formState: { errors },
+    } = useForm()
+
+    const onSubmit = (data) => console.log(data)
+
+
+
     return (
-        <div>
-            <textarea></textarea>
-        </div>
+        <>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <input {...register('name')} />
+                <input {...register('class')} />
+                <textarea {...register('desc')} />
+                <input type="submit" value={'save'} />
+            </form>
+
+            <button onClick={()=>console.log(watch())}>watch</button>-/
+        </>
     )
+
 }
